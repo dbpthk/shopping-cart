03823a50ba2196ed3cdbbd71697655a7e27ef958
Dhruv <dbpthk@gmail.com>
2026-01-20 19:53:44 +1100

    add addToCart, removeFromCart, updateQuantity, total method

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -37,4 +37,51 @@ export function useCart() {
       window.removeEventListener("storage", handleStorage);
     };
   }, []);
+
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.id === product.id);
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        );
+      }
+      return [...prevCart, { ...product, quantity: 1 }];
+    });
+  };
+
+  const removeFromCart = (productId) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
+  };
+
+  const updateQuanity = (productId, quantity) => {
+    if (quantity < 1) return;
+    setCart((prevCart) =>
+      prevCart.map((item) =>
+        item.id === productId ? { ...item, quantity } : item,
+      ),
+    );
+  };
+
+  //not required in react 19 or later
+  const total = useMemo(() => {
+    return Number(
+      cart
+        .reduce((sum, item) => {
+          const itemTotal = item.price * (item.quantity || 0);
+          return sum + itemTotal;
+        }, 0)
+        .toFixed(2),
+    );
+  }, [cart]);
+
+  return {
+    cart,
+    addToCart,
+    removeFromCart,
+    updateQuanity,
+    total,
+  };
 }

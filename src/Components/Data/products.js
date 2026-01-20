a11ecb8eabad8ee8f67fe7106784d57f88a2188a
Dhruv <dbpthk@gmail.com>
2026-01-20 17:37:35 +1100

    create project structure

---
+++ src/Components/Data/products.js
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    id: 1,
+    name: "Macbook Pro",
+    price: 1300,
+  },
+  {
+    id: 2,
+    name: "iPhone 13",
+    price: 999,
+  },
+  {
+    id: 3,
+    name: "AirPods Pro",
+    price: 249,
+  },
+  {
+    id: 4,
+    name: "iPad Pro",
+    price: 799,
+  },
+  {
+    id: 5,
+    name: "Apple Watch Series 7",
+    price: 399,
+  },
+];
